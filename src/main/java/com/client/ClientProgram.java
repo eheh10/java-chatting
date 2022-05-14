06a227774b1c05be6680d4ee3cb3951cac61e15b
eunhye park <lime1pm@naver.com>
2022-05-14 12:31:32 +0900

    Feat: ADD ClientProgram

---
+++ src/main/java/com/client/ClientProgram.java
@@ -0,0 +1,20 @@
+package com.client;
+
+import com.util.SocketUtil;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientProgram {
+    public static void main(String[] args) throws IOException {
+        String serverIp = "127.0.0.1";
+        Socket socket = new Socket(serverIp,7777);
+        String acceptTime = SocketUtil.nowTime();
+
+        Thread sender = new Thread(new ClientSender(socket,acceptTime));
+        Thread receiver = new Thread(new ClientReceiver(socket,acceptTime));
+
+        sender.start();
+        receiver.start();
+    }
+}
