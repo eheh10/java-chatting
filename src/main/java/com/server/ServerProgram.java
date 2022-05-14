8b22bd6b4d488cc91ad064f683c8064665fb82a8
eunhye park <lime1pm@naver.com>
2022-05-14 12:30:20 +0900

    Feat: ADD ServerProgram

---
+++ src/main/java/com/server/ServerProgram.java
@@ -0,0 +1,38 @@
+package com.server;
+
+import com.client.ChatClient;
+import com.util.SocketUtil;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServerProgram {
+    public static Map<String, ChatClient> clients = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(7777);
+
+        while(true){
+            Socket socket = serverSocket.accept();
+            String ip = SocketUtil.getIp(socket.getInetAddress());
+
+            String serverFileName = SocketUtil.nowTime() + "_" + ip;
+
+            if (!clients.containsKey(ip)) {
+                clients.put(ip, new ChatClient(serverFileName,socket));
+            }
+
+            System.out.println(ip+":"+socket.getPort()+"연결");
+
+            Thread sender = new Thread(new ServerSender());
+            Thread receiver = new Thread(new ServerReceiver(socket));
+
+            sender.start();
+            receiver.start();
+        }
+    }
+}
+
